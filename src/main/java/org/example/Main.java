35de8ef15eca261601692ffb30111f025371ec25
bohdan.napadovskyi <napadovskibohdan@gmail.com>
2023-06-09 17:36:14 +0300

    First commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.example.twoNumber.AddTwoNumbersSolution;
+import org.example.twoNumber.ListNode;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        System.out.println(new ValidParenthesesSolution().isValid("([)]"));
+//        System.out.println(new ValidParenthesesSolution().isValid("{[]}"));
+//        System.out.println(new ValidParenthesesSolution().isValid("()"));
+//        System.out.println(new ValidParenthesesSolution().isValid("()[]{}"));
+        System.out.println(new ValidParenthesesSolution().isValid("(]"));
+
+    }
+
+
+
+
+}
